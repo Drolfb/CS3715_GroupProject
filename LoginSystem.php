f8d09de93b3b155504485fdaabb7f5bdb73770c7
Fredmental <supervemon@hotmail.com>
2016-03-15 21:43:41 -0230

    Update LoginSystem.php

--- LoginSystem.php
+++ LoginSystem.php
@@ -1,27 +1,34 @@
 <!DOCTYPE html>
-<!-- Group Project, This is an super basic login/password system that will later
-     be updated with MySQL support. -->
+<!-- Group Project, This is a super basic login/password system that is with 
+     MySQL support. -->
 <html>
     <?php 
-        // A test array with username/password combinations
-        $usernamePassword = array (
-            "Fred" => "123456",
-            "Brian" => "654321"
-        );
-        
         // Grabbing the information the user types in the username and password
         // forms.
-        $inputUsername = $_GET["username"];
-        $inputPassword = $_GET["password"];
-         
-        echo "<p> Username=$inputUsername <br> Password=$inputPassword</p>";
+        $inputUsername = $_POST["username"];
+        $inputPassword = $_POST["password"];
+    
+        // MySQL username and password.
+        $MySQLusername = root;
+        $MySQLpassword = root;
+        
+        // Create database connection using PHP Data Object (PDO).
+        $db = new PDO("mysql:host=localhost;dbname=QuickDraw_Test", $MySQLusername, $MySQLpassword);
+        
+        // Name of the table we are using for the database.
+        $MySQLtable = 'UserInfo';
 
-        // Run through the $usernamePassword array and compare it with the user's
-        // input data. 
-        while (list($username, $value) = each($usernamePassword)) {
-            if ($username == $inputUsername) {
-                if ($value == $inputPassword) {
-                    echo "Login Successful!";
+        // Grabbing everything from the table
+        $userInfoTable = $db->query('SELECT * from '.$MySQLtable);
+        
+        // Closing the database connection.
+        $db = NULL;
+
+        // Run through the MySQL table  and compare it with the user's input data. 
+        while($rows = $userInfoTable->fetch()) {
+            if ($rows['Username'] == $inputUsername) {
+                if ($rows['Password'] == $inputPassword) {
+                    echo "Login Success!";
                 }
             }
         }
@@ -30,8 +37,8 @@
         <title>Project Login System</title>
     </head>
     <body>
-        <!-- Using Get for now, will use post in final version. -->
-        <form action="LoginSystem.php" method="get">
+        <!-- Using POST to get user information in a secure matter. -->
+        <form action="LoginSystem.php" method="post">
             <label>Username: </label><br>
             <input type="text" name="username"><br> 
             <label>Password: </label><br>
@@ -40,3 +47,24 @@
         </form>
     </body>
 </html>
+<?php
+/**
+ * Database Info
+ * mysql> DESCRIBE UserInfo;
++----------+----------+------+-----+---------+-------+
+| Field    | Type     | Null | Key | Default | Extra |
++----------+----------+------+-----+---------+-------+
+| ID       | int(11)  | NO   | PRI | NULL    |       |
+| Username | tinytext | NO   | UNI | NULL    |       |
+| Password | tinytext | NO   |     | NULL    |       |
++----------+----------+------+-----+---------+-------+
+
+mysql> SELECT * FROM UserInfo;
++----+----------+----------+
+| ID | Username | Password |
++----+----------+----------+
+|  0 | Fred     | 123456   |
+|  1 | Brian    | 654321   |
++----+----------+----------+
+ */
+?>
