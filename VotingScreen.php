66ce4f68e0c123f62d7dad7604deb27dd56750b5
Fredmental <supervemon@hotmail.com>
2016-03-27 17:03:23 -0230

    Update VotingScreen.php

--- VotingScreen.php
+++ VotingScreen.php
@@ -1,25 +1,22 @@
 <?php
         // MySQL username and password.
-	$MySQLusername = "root"; 
-	$MySQLpassword = "root";
-        $dataURL = "";
+	$MySQLusername = "cs3715_tb6774";  
+	$MySQLpassword = "purplesilver7";
         
 	// Create database connection using PHP Data Object (PDO).
 	// When in MUN, make it  $db = new PDO("mysql:host=mysql.cs.mun.ca;dbname=cs3715_tb6774", $MySQLusername, $MySQLpassword);
-	$db = new PDO("mysql:host=localhost;dbname=QuickDraw_Test", $MySQLusername, $MySQLpassword);
+	$db = new PDO("mysql:host=mysql.cs.mun.ca;dbname=cs3715_tb6774", $MySQLusername, $MySQLpassword);
 
 	// Name of the table we are using for the database.
-	$accountTable = 'AccountInfo'; // testUserInfo
-        $votingTable = 'VotingTest';
+        $gameTable = 'Game_abc';
 
 	// Grabbing everything from the table
-	$accounts = $db->query('SELECT * from '.$accountTable);
-        $votingInfo = $db->query('SELECT * FROM '.$votingTable);
+        $gameTableInfo = $db->query('SELECT * FROM '.$gameTable);
         
         // Closing the database connection.
         $db = NULL;
         
-        while($rows = $votingInfo->fetch()) {
+        while($rows = $gameTableInfo->fetch()) {
             $image = str_replace("+", "%2B", $rows['CanvasString']);
             $image = urldecode($image);
             echo '<img height="400" src= "'.$image.'">';
