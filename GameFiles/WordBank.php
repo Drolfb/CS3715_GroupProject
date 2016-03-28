90bdc354e8638e7de6854aa6ad627729798eb0f3
Fredmental <supervemon@hotmail.com>
2016-03-27 23:04:01 -0230

    Update WordBank.php

--- GameFiles/WordBank.php
+++ GameFiles/WordBank.php
@@ -2,28 +2,39 @@
         session_start();
     
         // MySQL username and password.
-        $MySQLusername = root; // cs3715_tb6774
-        $MySQLpassword = root; //purplesilver7
+        $MySQLusername = "cs3715_tb6774";
+        $MySQLpassword = "purplesilver7";
+        
+        $randomSentence = "";
         
         // Create database connection using PHP Data Object (PDO).
         // When in MUN, make it  $db = new PDO("mysql:host=mysql.cs.mun.ca;dbname=cs3715_tb6774", $MySQLusername, $MySQLpassword);
-        $db = new PDO("mysql:host=localhost;dbname=QuickDraw_Test", $MySQLusername, $MySQLpassword);
+        $db = new PDO("mysql:host=mysql.cs.mun.ca;dbname=cs3715_tb6774", $MySQLusername, $MySQLpassword);
         
         // Name of the table we are using for the database.
-        $MySQLtable = 'WordBank'; // testUserInfo
+        $MySQLtable = 'WordBank';
 
         // Grabbing everything from the table
-        $WordBank = $db->query('SELECT * from '.$MySQLtable. ' order by RAND() LIMIT 1');
+        $randomWord1 = $db->query('SELECT Words from '.$MySQLtable. ' ORDER BY RAND() LIMIT 1');
+        $randomWord2 = $db->query('SELECT Words from '.$MySQLtable. ' ORDER BY RAND() LIMIT 1');
         
         
         // Closing the database connection.
         $db = NULL;
             
         // Run through the MySQL table  and compare it with the user's input data. 
-        while($rows = $WordBank->fetch()) {
-            echo "rows[0]=" . $rows[0] . " rows[1]=" . $rows[1] . " rows[2]=" . $rows[2] . "<br>";
+        while($rows = $randomWord1->fetch()) {
+            echo "$rows[0] ";
+            $randomSentence = $rows[0] . " ";
+        }
+        
+        while($rows = $randomWord2->fetch()) {
+            echo " $rows[0] <br>";
+            $randomSentence = $randomSentence . $rows[0];
             
         }
+        
+        echo "The random sentence is: $randomSentence";
 echo '<html>';
 echo    '<head>';
 echo       '<title>Project Login System</title>';
