c4f4e9059def3cdb2530445e475e5fe2dba04a90
bdfagan <tudelord@hotmail.com>
2016-03-28 21:00:31 -0230

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,70 +1,47 @@
 <?php
 session_start();
+
 if (!isset($_SESSION['username'])) {
-	echo "You are not logged in!<br/>";
-	echo '<a href="./index.php">Return to Main Page</a>';
+	echo "<script>alert(\"You are not logged in!\"); window.location('./index.php')</script>";
 	die();
 }
+
+if (!isset($_GET['roomID'])) {
+	echo "Malformed request. Please return to the <a href=\"./index.php\">home page</a> and try again.";
+}
+
+$roomID = $_GET['roomID'];
+
 // MySQL username and password.
 $MySQLusername = "cs3715_tb6774"; 
 $MySQLpassword = "purplesilver7";
 
-// Create database connection using PHP Data Object (PDO).
+// Create database connection using PHP Data Object (PDO). Blatantly copy pasted from Freddie's code
 // When in MUN, make it  $db = new PDO("mysql:host=mysql.cs.mun.ca;dbname=cs3715_tb6774", $MySQLusername, $MySQLpassword);
 $db = new PDO("mysql:host=mysql.cs.mun.ca;dbname=cs3715_tb6774", $MySQLusername, $MySQLpassword);
 
 // Name of the table we are using for the database.
-$MySQLtable = 'AccountInfo'; // testUserInfo
+$MySQLtable = 'Rooms'; // change this to whatever the table name is
 
-// Grabbing everything from the table
+// Get row corresponding to room
 
-$userInfoTable = $db->query('SELECT * from '.$MySQLtable.' WHERE Username="'.$_SESSION['username'].'"');
-$username = "Error retrieving username";
-$password = "lolwut";
-$pwins = "Error retrieving wins";
-while($rows = $userInfoTable->fetch()) {
-    $username = $rows[1];
-    $pwins = $rows[3];
+$room = $db->query('SELECT * from '.$MySQLtable.' WHERE RoomID = '.$roomID);
+
+// Closing the database connection.
+$db = NULL;
+while (true) {
+	if ($room[1] != NULL && $room[2] != NULL && $room[3] != NULL) { //Assuming table layout is RoomID, Player1, Player2, Player3, this means table is full
+		//header("Location:BasicCanvas.php?roomID=".$roomID);	//GET request
+		echo $roomID;	//Redirect through AJAX, it's better this way
+		break;
+	}
+	else if (connection_aborted()) {
+		error_log ("Script was aborted by the user.");
+		die();		//Breakpoint in case user closed the connection
+		break;
+	}
+	else {
+		sleep(5);	//Wait 5 seconds and check again for as long as they're willing to wait
+	}
 }
 ?>
-<html>
-<head>
-	<title></title>
-	<link rel="stylesheet" href="style.css">
-	<script type="text/javascript">
-		  var uname = "<?php echo $username ?>";
-    var pword = "<?php echo $password ?>";
-    var pwins = "<?php echo $pwins ?>";
-    var prank = "Bronze";
-		function getProfile() {
-      if (pwins > 29) {
-          prank = "Platinum";
-      } else if (pwins > 19) {
-          prank = "Gold";
-      } else if (pwins > 9) {
-          prank = "Silver";
-      }
-      document.getElementsByTagName('title')[0].innerHTML = uname +"'s Profile - QuickDraw";
-      document.getElementById("name").innerHTML = uname + "'s Stats";
-      document.getElementById("tablew").innerHTML = pwins;
-      document.getElementById("tabler").innerHTML = prank;
-		}
-	</script>
-</head>
-<body onload="getProfile();">
-	<div id="btndiv">
-		<h1 id="name"></h1>
-		<table>
-			<tr>
-				<th>Wins</th>
-				<th>Rank</th>
-			</tr>
-			<tr>
-				<td id="tablew" class="stats"></td>
-				<td id="tabler" class="stats"></td>
-			</tr>
-		</table><br/>
-		<a href="./index.php">Back to main page</a>
-	</div>
-</body>
-</html>
